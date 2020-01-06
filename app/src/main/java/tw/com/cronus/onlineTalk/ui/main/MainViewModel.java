8ffcb153e70f8fde9da0d000b4bcf77f7c092b9e
Cronus <seer20582609@gmail.com>
2020-01-07 00:03:00 +0800

    build view model

--- app/src/main/java/tw/com/cronus/onlineTalk/ui/main/MainViewModel.java
+++ app/src/main/java/tw/com/cronus/onlineTalk/ui/main/MainViewModel.java
@@ -1,7 +1,20 @@
 package tw.com.cronus.onlineTalk.ui.main;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseUser;
+
 class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final MutableLiveData<FirebaseUser> userLiveData = new MutableLiveData<>();
+
+    public LiveData<FirebaseUser> getUser() {
+        return userLiveData;
+    }
+
+//    public void setUser(FirebaseUser user) {
+//        userLiveData = user;
+//    }
+
 }
